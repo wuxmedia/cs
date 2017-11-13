5c6dbc9633bb00da2c42fe1acfd526d6de5406d3
Tom Hardy <wuxmedia@gmail.com>
2017-11-13 10:32:09 +0000

    slight rewrite
    used curl instead of httpie

--- cs.sh
+++ cs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-### domain overview
 #get name from command line:
 DOM=$1
+echo -e "\e[35mName Servers: \e[39m"
 echo "$(dig NS "$DOM" +short)"
+echo -e '\e[35mHOST:\e[39m' 
 echo "$(host "$DOM")"
-#isitup?
-echo "$(http "$DOM" --headers | head -n4)"
+echo -e '\e[35mREVERSE LOOKUP:\e[39m'
+echo "$(host $(dig A $DOM +short))"
+echo -e '\e[35mHTTP REQUEST:\e[39m'
+echo "$(curl -I $DOM 2> /dev/null| grep -e "HTTP" -e "Location" )"
