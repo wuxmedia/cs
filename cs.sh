47684dd6e2ff42756eb04b8a3a2970af680c6c5d
Tom Hardy <wuxmedia@gmail.com>
2017-11-13 10:51:54 +0000

    Update cs.sh
    really not sure what all those 'echos' were for.

--- cs.sh
+++ cs.sh
@@ -2,10 +2,10 @@
 #get name from command line:
 DOM=$1
 echo -e "\e[35mName Servers: \e[39m"
-echo "$(dig NS "$DOM" +short)"
+dig NS "$DOM" +short
 echo -e '\e[35mHOST:\e[39m' 
-echo "$(host "$DOM")"
+host "$DOM"
 echo -e '\e[35mREVERSE LOOKUP:\e[39m'
-echo "$(host $(dig A $DOM +short))"
+host $(dig A $DOM +short)
 echo -e '\e[35mHTTP REQUEST:\e[39m'
-echo "$(curl -I $DOM 2> /dev/null| grep -e "HTTP" -e "Location" )"
+curl -I $DOM 2> /dev/null| grep -e "HTTP" -e "Location"
