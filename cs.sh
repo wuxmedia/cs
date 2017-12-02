8ff65f6fe75b9213014411dc9f90cbc1960d3dee
Tom Hardy <wuxmedia@gmail.com>
2017-12-02 13:16:04 +0000

    Update cs.sh
    I should really learn to use git as it was intended..

--- cs.sh
+++ cs.sh
@@ -2,14 +2,15 @@
 ### domain overview tool
 #get name from command line and strip http(s):// from it:
 DOM=$(echo "$1" | sed 's~http[s]*://~~g')
-echo -e "\e[35mName Servers: \e[39m"
+echo -e "\e[35mNAME SERVERS: \e[39m"
 dig NS "$DOM" +short
 echo -e '\e[35mHOST:\e[39m' 
 host "$DOM"
 echo -e '\e[35mREVERSE LOOKUP:\e[39m'
 host $(dig A "$DOM" +short)
 echo -e '\e[35mPRIMARY MX LOOKUP:\e[39m'
-dig A +short $(dig MX "$DOM" +short | grep -oE '[^ ]+$' | head -n1)
+MX=$(dig MX "$DOM" +short | sort -n | grep -oE '[^ ]+$' |  head -n1)
+echo -n "($MX) "
+dig A +short $(dig MX "$MX" +short | grep -oE '[^ ]+$' | head -n1)
 echo -e '\e[35mHTTP REQUEST:\e[39m'
 curl --max-time 5 -sI "$DOM" | grep -e "HTTP" -e "Location"
-
