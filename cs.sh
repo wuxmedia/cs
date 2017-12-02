8716ebcdde2d324afaefdf51d9e84cd45586bc42
Tom Hardy <wuxmedia@gmail.com>
2017-12-02 13:00:43 +0000

    Update cs.sh
    bit of a rewtrite, uses more dig-ing and better curl-ing

--- cs.sh
+++ cs.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+### domain overview tool
 #get name from command line and strip http(s):// from it:
-DO=$1
-DOM=$(echo "$DO" | sed 's~http[s]*://~~g')
+DOM=$(echo "$1" | sed 's~http[s]*://~~g')
 echo -e "\e[35mName Servers: \e[39m"
 dig NS "$DOM" +short
 echo -e '\e[35mHOST:\e[39m' 
 host "$DOM"
 echo -e '\e[35mREVERSE LOOKUP:\e[39m'
-host $(dig A $DOM +short)
+host $(dig A "$DOM" +short)
+echo -e '\e[35mPRIMARY MX LOOKUP:\e[39m'
+dig A +short $(dig MX "$DOM" +short | grep -oE '[^ ]+$' | head -n1)
 echo -e '\e[35mHTTP REQUEST:\e[39m'
-curl -sI $DOM | grep -e "HTTP" -e "Location"
+curl --max-time 5 -sI "$DOM" | grep -e "HTTP" -e "Location"
+
