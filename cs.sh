8fdd9e847e5203fb4daa86e2e575ef02c7a478fb
Tom Hardy <wuxmedia@gmail.com>
2017-12-02 13:29:24 +0000

    Update cs.sh
    extraneous grep is gone

--- cs.sh
+++ cs.sh
@@ -11,6 +11,6 @@ host $(dig A "$DOM" +short)
 echo -e '\e[35mPRIMARY MX LOOKUP:\e[39m'
 MX=$(dig MX "$DOM" +short | sort -n | grep -oE '[^ ]+$' |  head -n1)
 echo -n "($MX) "
-dig A +short $(dig MX "$MX" +short | grep -oE '[^ ]+$' | head -n1)
+dig A +short $(dig MX "$MX" +short | head -n1)
 echo -e '\e[35mHTTP REQUEST:\e[39m'
 curl --max-time 5 -sI "$DOM" | grep -e "HTTP" -e "Location"
