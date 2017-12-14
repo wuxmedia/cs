47be7f904e54e1fe8daa8dcc7f24352c2bba3bd5
Tom Hardy <wuxmedia@gmail.com>
2017-12-14 11:21:20 +0000

    fixed broken thing

--- cs.sh
+++ cs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 ### domain overview tool
+if [ "$*" == "" ]; then
+    echo "Please supply a domain to check"
+    exit 1
+fi
 #get name from command line and strip http(s):// from it:
 DOM=$(echo "$1" | sed 's~http[s]*://~~g')
 echo -e "\e[35mNAME SERVERS: \e[39m"
@@ -9,9 +13,7 @@ host "$DOM"
 echo -e '\e[35mREVERSE LOOKUP:\e[39m'
 host $(dig A "$DOM" +short)
 echo -e '\e[35mPRIMARY MX LOOKUP:\e[39m'
-MX=$(dig MX "$DOM" +short | sort -n | grep -oE '[^ ]+$' |  head -n1)
-echo -n "($MX) "
-dig A +short $(dig MX "$MX" +short | head -n1)
+dig A +short $(dig MX "$DOM" +short | sort -n  | head -n1)
 echo -e '\e[35mHTTP REQUEST:\e[39m'
 curl --max-time 5 -sI "$DOM" | grep -e "HTTP" -e "Location"
 echo -e '\e[35mSSL INFO:\e[39m'
