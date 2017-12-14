fe9381cf6440a24134f5047aada606aa7c63fa4b
Tom Hardy <wuxmedia@gmail.com>
2017-12-14 10:17:12 +0000

    added ssl options

--- cs.sh
+++ cs.sh
@@ -14,4 +14,5 @@ echo -n "($MX) "
 dig A +short $(dig MX "$MX" +short | head -n1)
 echo -e '\e[35mHTTP REQUEST:\e[39m'
 curl --max-time 5 -sI "$DOM" | grep -e "HTTP" -e "Location"
-#test
+echo -e '\e[35mSSL INFO:\e[39m'
+echo|openssl s_client -servername $DOM -connect $DOM:443 2>/dev/null | openssl x509 -noout -text | grep -e DNS: -e Subject: -e "Not "  -e Issuer: | sed -e 's/^[ \t]*//'
