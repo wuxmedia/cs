c5b013770db4fb6fa04301e6b1413f9eeeb625a0
Tom Hardy <wuxmedia@gmail.com>
2025-04-22 07:59:31 +0100

    Update cs.sh
    macos version

--- cs.sh
+++ cs.sh
@@ -6,15 +6,19 @@ if [ "$*" == "" ]; then
 fi
 #get name from command line and strip http(s):// from it and any trailing slashes:
 DOM=$(echo "$1" | sed 's~http[s]*://~~g' | sed 's:/*$::' )
-echo -e "\e[35mNAME SERVERS: \e[39m"
+echo -e "NAME SERVERS:"
 dig NS "$DOM" +short
-echo -e '\e[35mHOST:\e[39m' 
+echo -e "CF NAME SERVERS:"
+dig NS @ns1.cloudflare.com "$DOM" +short
+echo -e "CF A RECORDS:"
+dig A @ns1.cloudflare.com "$DOM" +short
+echo -e 'HOST:' 
 host "$DOM"
-echo -e '\e[35mREVERSE LOOKUP:\e[39m'
-host $(dig A "$DOM" +short)
-echo -e '\e[35mPRIMARY MX LOOKUP:\e[39m'
-dig A +short $(dig MX "$DOM" +short | sort -n  | head -n1) | head -n1
-echo -e '\e[35mHTTP REQUEST:\e[39m'
+echo -e 'DNSSEC?:' 
+dig DS "$DOM" +short
+whois "$DOM" | grep DNSSEC | head -n1
+echo -e 'HTTP REQUEST:'
 curl --max-time 5 -sI "$DOM" | grep -e "HTTP" -e "Location"
-echo -e '\e[35mSSL INFO:\e[39m'
+curl --max-time 5 -sI "$DOM" | grep -ie "x-kinsta-cache"
+echo -e 'SSL INFO:'
 echo|openssl s_client -servername $DOM -connect $DOM:443 2>/dev/null | openssl x509 -noout -text | grep -e DNS: -e Subject: -e "Not "  -e Issuer: | sed -e 's/^[ \t]*//'
